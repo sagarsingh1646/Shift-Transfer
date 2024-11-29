c86ec60690050f875d57fc3ee671981aa47c573c
Sagar Singh <sagarsingh@Sagars-MacBook-Pro.local>
2024-11-29 14:39:03 +0530

    Dashboard added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TaskCard from './components/TaskCard'
+import DashBoard from './pages/DashBoard'
 
 function App() {
   return (
     <>
       <div className='font-bold'>World</div>
+      <DashBoard />
     </>
   )
 }
